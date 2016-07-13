0f7dcf742e8821a5970365667843c0d9d7a7167e
mrexodia <mr.exodia.tpodt@gmail.com>
2016-07-13 07:58:49 +0200

    fix?

--- crowdin-sync.bat
+++ crowdin-sync.bat
@@ -1,7 +1,7 @@
 @echo off
 SET PATH=%PATH%;c:\Qt\qt-5.6.0-x64-msvc2013\5.6\msvc2013_64\bin;c:\Program Files (x86)\7-Zip
-curl https://api.crowdin.com/api/project/x64dbg/export?key=%CROWDIN_API_KEY%
-curl -o translations.zip https://api.crowdin.com/api/project/x64dbg/download/all.zip?key=%CROWDIN_API_KEY%
+curl http://api.crowdin.com/api/project/x64dbg/export?key=%CROWDIN_API_KEY%
+curl -o translations.zip http://api.crowdin.com/api/project/x64dbg/download/all.zip?key=%CROWDIN_API_KEY%
 rmdir /S /Q translations
 7z x -otranslations translations.zip
 cd translations
