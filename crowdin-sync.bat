7ebd882673a5becb3a17adcc8d1de5b4a4b5a4bd
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2024-03-08 16:00:33 +0100

    Fully switch to crowdin-cli

--- crowdin-sync.bat
+++ crowdin-sync.bat
@@ -1,22 +1,20 @@
 @echo off
-if "%QT64PATH%"=="" set QT64PATH=c:\Qt\qt-5.6.2-x64-msvc2013\5.6\msvc2013_64\bin
-SET PATH=%PATH%;%QT64PATH%;c:\Program Files\7-Zip
-del /S /Q *.qm >nul 2>&1
-curl -s -k https://api.crowdin.com/api/project/x64dbg/export?key=%CROWDIN_API_KEY%
-curl -s -k -o translations.zip https://api.crowdin.com/api/project/x64dbg/download/all.zip?key=%CROWDIN_API_KEY%
+
+echo Uploading English sources to Crowdin...
+java -jar crowdin-cli.jar upload sources
+if not %ERRORLEVEL%==0 exit /b
+
+echo Downloading translations from Crowdin...
 rmdir /S /Q translations >nul 2>&1
-7z x -otranslations translations.zip >nul 2>&1
+java -jar crowdin-cli.jar download
+if not %ERRORLEVEL%==0 exit /b
+
+echo Checking translations...
 TranslationChecker.exe translations --folder --fix
 set CHECKER_ERRORLEVEL=%ERRORLEVEL%
-cd translations
-for /D %%a in (*) do (set fname=%%a) & call :rename
-move *.qm ..\ >nul 2>&1
-cd ..
+
+echo Generating Qt translations...
+for %%f in (translations\*.ts) do (
+    lrelease -nounfinished %%f
+)
 exit /b %CHECKER_ERRORLEVEL%
-
-goto :eof
-
-:rename
-set trname=x64dbg_%fname:-=_%.ts
-copy %fname%\x64dbg.ts %trname% >nul 2>&1
-lrelease -nounfinished %trname%
