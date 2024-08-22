f196f36c5d4e472a8a9bfe80abda77fb045a38f5
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2024-08-22 20:39:01 +0200

    Reduce output spam from crowdin-cli

--- crowdin-sync.bat
+++ crowdin-sync.bat
@@ -1,13 +1,19 @@
 @echo off
 
 echo Uploading English sources to Crowdin...
-java -jar crowdin-cli.jar upload sources --no-colors --no-progress
-if not %ERRORLEVEL%==0 exit /b
+java -jar crowdin-cli.jar upload sources --no-colors --no-progress > upload.log 2>&1
+if not %ERRORLEVEL%==0 (
+    type upload.log
+    exit /b 1
+)
 
 echo Downloading translations from Crowdin...
 rmdir /S /Q translations >nul 2>&1
-java -jar crowdin-cli.jar download --no-colors --no-progress
-if not %ERRORLEVEL%==0 exit /b
+java -jar crowdin-cli.jar download --no-colors --no-progress > download.log 2>&1
+if not %ERRORLEVEL%==0 (
+    type download.log
+    exit /b 1
+)
 
 echo Checking translations...
 TranslationChecker.exe translations --fix
